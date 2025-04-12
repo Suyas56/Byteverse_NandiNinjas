4dd5596948668df2f5109b6ccc0f7566e353e798
suyash35565 <suyashv.ug23.cs@nitp.ac.in>
2025-04-13 02:17:05 +0530

    revert chages

--- BookHive/vite.config.js
+++ BookHive/vite.config.js
@@ -1,27 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext', // Use modern JavaScript syntax for smaller builds
-    minify: false, // Disable minification to reduce memory usage
-    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Split vendor libraries into separate chunks
-          vendor: ['react', 'react-dom', 'redux', 'react-redux'],
-        },
-      },
-    },
-  },
 })
